d8ea674d4a2ee0747aeba2f2eefe9082df89c44d
Arlex Saavedra <arlexdavidsaavedra@hotmail.com>
2019-05-17 15:42:22 -0500

    Este es el primer commit del proyecto y solo esta funcionando la parte del pais

---
+++ sistema.backend/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.pais (id_pais, pais) VALUES (1, 'Colombia');
+INSERT INTO public.pais (id_pais, pais) VALUES (2, 'Venezuela');
+INSERT INTO public.pais (id_pais, pais) VALUES (3, 'Peru');
+INSERT INTO public.pais (id_pais, pais) VALUES (4, 'Ecuador');
+INSERT INTO public.pais (id_pais, pais) VALUES (5, 'Chile');
+INSERT INTO public.pais (id_pais, pais) VALUES (6, 'Argentina');
+INSERT INTO public.pais (id_pais, pais) VALUES (7, 'Brasil');
+INSERT INTO public.pais (id_pais, pais) VALUES (8, 'Uruguay');
+INSERT INTO public.pais (id_pais, pais) VALUES (9, 'Paraguay');
